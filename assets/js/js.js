44cf6e75d54c58628bb93c59ff330899b2cc9ad5
Benjamin J. Balter <ben@balter.com>
2012-12-24 11:58:20 -0500

    minify js

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,8 +2,6 @@
 layout: none
 ---
 {% include js/lib/emphasis.js %}
-{% include js/lib/jquery.infinitescroll.js %}
-{% include js/lib/jquery.relatize_date.js %}
 {% include js/lib/underscore.js %}
 {% include js/lib/backbone.js %}
 {% include js/lib/bootstrap.js %}
