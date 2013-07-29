8f6e6ddfed1d6179bb9a1ff8c36ef4f9432058c1
Ben Balter <ben.balter@github.com>
2013-07-28 20:12:37 -0700

    bump to bs 3.0

--- _includes/js/load-impress.js
+++
@@ -1,1 +0,0 @@
-jQuery('#collab_img').click( function() { jQuery(this).fadeOut(); jQuery('#collab_iframe').attr( 'src', 'http://ben.balter.com/wordpress-as-a-collaboration-platform/' ).fadeIn(); } );
--- assets/js/js.js
+++
@@ -1,7 +0,0 @@
----
-layout: none
----
-{% include js/lib/emphasis.js %}
-{% include js/lib/bootstrap.js %}
-{% include js/lib/jquery.infinitescroll.js %}
-{% include js/google-analytics.js %}
