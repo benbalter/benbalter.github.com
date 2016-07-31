593f0c975151b4066de75645d1c481b07b506459
Ben Balter <ben.balter@github.com>
2016-07-31 12:36:45 -0400

    move amazon link check to its own file

---
+++ spec/amazon_link_check.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+class AmazonLinkCheck < HTMLProofer::Check
+  def affiliate_id
+    @affiliate_id ||= YAML.load_file('_config.yml')['amazon']['affiliates_tag']
+  end
+
+  def uri
+    URI.parse(@link.href)
+  end
+
+  def params
+    CGI.parse(uri.query) if uri.query
+  end
+
+  def amazon_link?
+    uri.host =~ /amazon\.com/
+  end
+
+  def afiiliate_id?
+    return false unless params && params.key?('tag')
+    params['tag'].first == affiliate_id
+  end
+
+  def run
+    @html.css('a').each do |node|
+      @link = create_element(node)
+      next if @link.data_proofer_ignore || @link.href.nil?
+
+      if amazon_link? && !afiiliate_id?
+        msg = "Misssing Amazon Affiliate ID: #{@link.href}"
+        add_issue(msg, line: node.line)
+      end
+    end
+  end
+end
